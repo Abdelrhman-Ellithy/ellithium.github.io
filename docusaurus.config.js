f4af4f0effe8522d35535bd32e8db4c1c4f4c249
Abdelrhman-Ellithy <abdelarhmanellithy@gmail.com>
2025-05-15 16:01:21 +0300

    deploy the website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ellithium', // Usually your GitHub org/user name.
+  organizationName: 'Abdelrhman-Ellithy', // Usually your GitHub org/user name.
   projectName: 'ellithium.github.io', // Usually your repo name.
 
   onBrokenLinks: 'warn',
