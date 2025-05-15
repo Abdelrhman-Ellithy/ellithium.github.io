6240f181df09c06533c88a1229bdef899b2127e3
Abdelrhman-Ellithy <abdelarhmanellithy@gmail.com>
2025-05-15 16:13:13 +0300

    deploy the website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://ellithium.github.io',
+  url: 'https://abdelrhman-ellithy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ellithium.github.io/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
